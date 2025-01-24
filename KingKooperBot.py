ce272d0ef3b2a755903b199fad969cb748b5e6b8
WanderingBoots <amstrauz@gmail.com>
2025-01-23 19:51:16 -0500

    Big update

--- KingKooperBot.py
+++ KingKooperBot.py
@@ -14,12 +14,13 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
-
 Magic_Words = [
     'wahoo',
 ]
 
+
 class WahooBoard:
+    num_of_words_found = 0
     def __init__(self, file_path='./KingKooper/counter.json'):
             self.file_path = file_path
 
@@ -35,45 +36,49 @@ class WahooBoard:
         key = str(member.guild.id) + "_" + str(member.id)
         counter_json = self.load_data()
         global current_user_count 
-        current_user_count = counter_json.get(key, 0)
-        counter_json[key] = current_user_count + 1
+        current_user_count = counter_json.get(key, 0) + WahooBoard.num_of_words_found
+        counter_json[key] = current_user_count 
+        WahooBoard.num_of_words_found = 0
         self.save_data(counter_json)
+
+    def reset_counter(self, member: discord.Member) -> None:
+        key = str(member.guild.id) + "_" + str(member.id)
+        counter_json = self.load_data()
+        counter_json[key] = 0
+        self.save_data(counter_json)
+
 
     async def handle_message(self, message: discord.Message) -> bool:
         if self.contains_the_word(message.content):
             self.update_counter(message.author)
-            if current_user_count < 100:
-                response = f'{message.author} - ' + str(current_user_count) + '/100 Wahoo\'s logged'
+            if current_user_count < 5:
+                response = '-# ' + f'{message.author} - ' + str(current_user_count) + '/100 Wahoo\'s logged'
             else:
-               response = f'{message.author} - ' + str(current_user_count) + '/100 Wahoo\'s logged \n'
-               'MARIO FORGIVENESS'
+               response = '-# ' + f'{message.author} - ' + str(current_user_count) + "/100 Wahoo\'s logged \n" + "## MARIO FORGIVENESS"
+               await message.author.remove_roles(role)
+               self.reset_counter(message.author)
             await message.channel.send(response)
-            #- TODO: Remove the role and reset counter
         await bot.process_commands(message)
     
-    async def get_info(self, member: discord.Member) -> int:
-        key = str(member.guild.id) + "_" + str(member.id)
-        counter_json = self.load_data()
-        return counter_json.get(key, 0)
-
-
     @staticmethod
     def contains_the_word(
             text: str
     ) -> bool:
         for word in Magic_Words:
             if word.lower() in text.lower():
+                WahooBoard.num_of_words_found = text.lower().count(word)
                 return True
             else:
                 continue
-
         return False
     
-leaderboard = WahooBoard()
+wahooboard = WahooBoard()
     
 @bot.event
 async def on_ready():
+    global role
     guild = discord.utils.get(bot.guilds, name=GUILD)
+    role = discord.utils.get(guild.roles, name='Mario Jail')
     print(
         f'{bot.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
@@ -83,7 +88,9 @@ async def on_ready():
 async def on_message(message: discord.Message):
     if message.author == bot.user:
         return
-    await leaderboard.handle_message(message)
-    
+    if message.channel.name == 'marios-jail-non-cannon':
+        await wahooboard.handle_message(message)
+
+
 
 bot.run(TOKEN)
